4d9d3d46e63b2e0d966dca5933e104940caca467
Opeyemi <Ope.adet@gmail.com>
2022-06-20 22:09:02 -0600

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,4 +1,6 @@
+const { Types } = require('mysql');
 const Sequelize = require ('sequelize');
 const config = new Sequelize('w47ya69v92bjuqie', 'jg2a9gl405m55zf5', 'tw6nal2atinb30jp',{ dialect: 'mysql', host: 'qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com' });
 
 module.exports = config;
+Types
